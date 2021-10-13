23f4b0e52fbb5939221f3c6934b8e540d2a96448
Giovanni maria Scala <gscala@e4r5p4.42roma.it>
2021-10-13 16:42:43 +0200

    cpp_07 almost done

--- CPP_07/ex02/Array.hpp
+++ CPP_07/ex02/Array.hpp
@@ -13,14 +13,39 @@ class Array{
         unsigned int length;
     public:
         //construction with no parameters
-        Array();
-
-        //construction with insigned int as parameter
-        Array(unsigned int n)
+        unsigned int getLength(void)
         {
+            return (length);
+        }
+        Array() : array(NULL), length(0) {};
+
+        //construction with unsigned int as parameter
+        Array(unsigned int n)        {
             this->length = n;
             array = new T[length];
+            for (unsigned int i = 0; i < length; i++)
+                array[i] = 0;
         };
+        const Array& operator=(const Array&arr)
+        {
+            this->length = arr.length;
+            array = new T[length];
+            for (int i = 0; i < length; i++)
+                array[i] = arr[i];
+            return(*this);
+        }
+
+        Array(Array &arr)
+	    {
+		    T *tempCopy;
+		    tempCopy = new T[arr.length];
+		    for (unsigned int i = 0; i < arr.length; i++)
+		    	tempCopy[i] = arr.array[i];
+		    length = arr.length;
+		    this->array = tempCopy;
+	    };
+
+        
 };
 
 #endif
--- CPP_07/ex02/main.cpp
+++ CPP_07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gscala <gscala@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/13 13:48:33 by gscala            #+#    #+#             */
-/*   Updated: 2021/10/13 14:45:41 by gscala           ###   ########.fr       */
+/*   Updated: 2021/10/13 16:40:42 by gscala           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,21 @@
 
 int main(){
     unsigned int n = 5;
+
+    std::cout << "Constructing an array with no parameters and printing out length: " << std::endl;
+    Array<int>array0;
+    std::cout << "length: " << array0.getLength() << std::endl;
+
+
+    std::cout << "Constructing array with unsigned int as parameter and printing out lenght: " << std::endl;
+    Array<int> ar(n);
+    std::cout << "length: " << ar.getLength() << std::endl;
+
+    std::cout << "Constructing new_array with = operator overload: " << std::endl;
+    Array<int> new_array = ar;
+    std::cout << "length: " << new_array.getLength() << std::endl;
+
+    std::cout << "using copy constructor: " << std::endl;
+    Array<int>copy_array(ar);
+    std::cout << "length: " << copy_array.getLength() << std::endl;
 }
